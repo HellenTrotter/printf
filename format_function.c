b33350219b4c2da0b1154ed89fb41eb3f7d9b1ce
KabeloSam <kabelosammykay@gmail.com>
2023-09-16 08:40:12 -0700

    _printf.c

---
+++ format_function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "main.h"
+/*_printf- produces output according to a format
+ *@format :string of characters
+ *Return : number of characters printed excluding the null
+ *
+ */
+
+int _printf(const char *format, ...)
+{
+va_list my_arguments;
+int i;
+
+va_start(my_arguments, format);
+}
