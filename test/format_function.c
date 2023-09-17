1a0cc6b20c1e1d984d9fa50b1f508386f2209f50
KabeloSam <kabelosammykay@gmail.com>
2023-09-17 07:40:55 -0700

    printf.c

--- test/format_function.c
+++ test/format_function.c
@@ -3,21 +3,25 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
-
 /**
 *_printf- produces output according to a format
 *@format :string of characters
 *Return: return number of characters printed
 */
-
+int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 va_list my_arguments;
 int i;
 char *s;
 int count_mychar = 0;
-
 va_start(my_arguments, format);
+if (format == NULL)
+{
+return (-1);
+}
+else
+{
 for (i = 0; (format[i] != '\0'); i++)
 {
 if (format[i] == '%')
@@ -26,25 +30,31 @@ i++;
 if (format[i] == 'c')
 {
 int c = va_arg(my_arguments, int);
-putchar(c);
+char ch = c;
+write(1, &ch, 1);
 count_mychar++;
 }
 else if (format[i] == 's')
 {
 s = va_arg(my_arguments, char *);
-fputs(s, stdout);
+if (s != NULL)
+{
+write(1, s, strlen(s));
 count_mychar += strlen(s);
+}
 }
 else if (format[i] == '%')
 {
-putchar('%');
+char symbol = '%';
+write(1, &symbol, 1);
 count_mychar++;
 }
 }
 else
 {
-putchar(format[i]);
-count_mychar ++;
+write(1, &format[i], 1);
+count_mychar++;
+}
 }
 }
 va_end(my_arguments);
--- test/format_function2.c
+++ test/format_function2.c
@@ -3,25 +3,21 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include "main.h"
+
 /**
 *_printf- produces output according to a format
 *@format :string of characters
 *Return: return number of characters printed
 */
-int _printf(const char *format, ...);
+
 int _printf(const char *format, ...)
 {
 va_list my_arguments;
 int i;
 char *s;
 int count_mychar = 0;
+
 va_start(my_arguments, format);
-if (format == NULL)
-{
-return (-1);
-}
-else
-{
 for (i = 0; (format[i] != '\0'); i++)
 {
 if (format[i] == '%')
@@ -30,31 +26,25 @@ i++;
 if (format[i] == 'c')
 {
 int c = va_arg(my_arguments, int);
-char ch = c;
-write(1, &ch, 1);
+putchar(c);
 count_mychar++;
 }
 else if (format[i] == 's')
 {
 s = va_arg(my_arguments, char *);
-if (s != NULL)
-{
-write(1, s, strlen(s));
+fputs(s, stdout);
 count_mychar += strlen(s);
-}
 }
 else if (format[i] == '%')
 {
-char symbol = '%';
-write(1, &symbol, 1);
+putchar('%');
 count_mychar++;
 }
 }
 else
 {
-write(1, &format[i], 1);
-count_mychar++;
-}
+putchar(format[i]);
+count_mychar ++;
 }
 }
 va_end(my_arguments);
