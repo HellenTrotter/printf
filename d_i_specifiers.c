9b131cfc0b16f55593dce2941b36ab98a73962d9
HellenTrotter <m.hbodiba@gmail.com>
2023-09-19 09:49:42 -0700

    task 1

---
+++ d_i_specifiers.c
@@ -0,0 +1,73 @@
+#include "main.h"
+#include <stdarg.h>
+/**
+ * _printf - produces output according to a format.
+ * @format: is a character string
+ * Return: number of characters
+ */
+int _printf(const char *format, ...);
+int _printf(const char *format, ...)
+{
+	const char *s = format;
+	int i;
+	int k;
+        char str[12];
+
+	va_list arg;
+
+	va_start(arg, format);
+
+	while (*s != '\0')
+	{
+		if (*s != '%')
+		{
+			write(1, s, 1);
+			s++;
+		}
+		else
+		{
+			s++;
+			if (*s == 'd' || *s == 'i')
+			{
+				int j = va_arg(arg, int);
+				
+				if (j == 0)
+				{
+					str[k++] = '0';
+				}
+				else {
+
+				if (j < 0)
+				{
+					str[k++] = '-';
+					j = -j;
+				}
+
+				while (j > 0)
+				{
+					str[k++] = '0' + (j %10);
+					j /= 10;
+				}
+				}
+
+				str[k] = '\0';
+				
+				for (i = 0; i < k / 2; i++)
+				{
+					char tmp = str[i];
+					str[i] = str[k - 1 - i];
+					str[k - 1 - i] = tmp;
+				}
+				
+				write(1, str, k);
+				s++;
+			}
+		}
+		write(1, s - 1, 2);
+		s += 2;
+
+	}
+	va_end(arg);
+	return (0);
+}
+
